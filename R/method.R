072bd1993e69e7d43c9fa25b26a3477bd2385940
Jim Hester <james.f.hester@gmail.com>
2021-02-18 13:12:33 -0500

    Support inheritance in method lookup

--- R/method.R
+++ R/method.R
@@ -1,24 +1,34 @@
 #' Retrieve or register an r7 method for a generic
 #'
 #' @param generic The generic to retrieve or register
-#' @param class The class to register the method for
+#' @param class,classes The class to register the method for
 #' @param value The new function to use for the method.
 #' @export
-method <- function(generic, class) {
-  if (inherits(class, "r7_class")) {
-    class <- class@name
+method <- function(generic, classes) {
+  if (inherits(classes, "r7_class")) {
+    classes <- class_names(classes)
   }
-  get_r7_method(generic, class, envir = parent.frame())
+
+  i <- 1
+  fun <- get_r7_method(generic, classes[[i]], envir = parent.frame())
+  while(is.null(fun) && i < length(classes)) {
+    i <- i + 1
+    fun <- get_r7_method(generic, classes[[i]], envir = parent.frame())
+  }
+  if (is.null(fun)) {
+    stop(sprintf("No methods defined for generic '%s' with classes:\n%s", generic, paste0("- ", classes,  collapse = "\n"), call. = FALSE))
+  }
+  fun
+}
+
+get_r7_method <- function(generic, class, envir) {
+  # We lookup the registered method in the S3 methods table directly, as R7 generics don't use UseMethod.
+  s3_table <- get(".__S3MethodsTable__.", envir)
+  get0(paste0(generic, ".", class), s3_table)
 }
 
 #' @rdname method
 #' @export
 method_register <- function(generic, class, value) {
   registerS3method(generic, class, value, envir = parent.frame())
-}
-
-get_r7_method <- function(generic, class, envir) {
-  # We lookup the registered method in the S3 methods table directly, as R7 generics don't use UseMethod.
-  s3_table <- get(".__S3MethodsTable__.", envir)
-  get(paste0(generic, ".", class), s3_table)
 }
--- tests/testthat/test-method.R
+++ tests/testthat/test-method.R
@@ -1,7 +1,16 @@
 test_that("methods can be registered for a generic and then called", {
   text <- class_new("text", parent = "character", constructor = function(text = character()) object_new(.data = text))
   foo <- r7_generic(name = "foo", signature = alist(x=))
-  method_register("foo", "text", function(x) x@.data)
+  method_register("foo", "text", function(x) paste0("foo-", x@.data))
 
-  expect_equal(foo(text("bar")), "bar")
+  expect_equal(foo(text("bar")), "foo-bar")
+})
+
+test_that("single inheritance works when searching for methods", {
+  text <- class_new("text", parent = "character", constructor = function(text = character()) object_new(.data = text))
+  foo2 <- r7_generic(name = "foo2", signature = alist(x=))
+
+  method_register("foo2", "character", function(x) paste0("foo2-", x))
+
+  expect_equal(foo2(text("bar")), "foo2-bar")
 })
