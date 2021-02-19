a4547d97ade7437d1a516126dc52b67a2ce0420d
Jim Hester <james.f.hester@gmail.com>
2021-02-19 12:47:39 -0500

    Cut out some more fat from `method()`

--- R/method.R
+++ R/method.R
@@ -5,22 +5,16 @@
 #' @param value The new function to use for the method.
 #' @export
 method <- function(generic, signature) {
-  if (!is.list(signature)) {
-    signature <- list(signature)
-  }
+  #if (!is.list(signature)) {
+    #signature <- list(signature)
+  #}
   for (i in seq_along(signature)) {
-    if (inherits(signature[[i]], "r7_class")) {
+    #if (inherits(signature[[i]], "r7_class")) {
       signature[[i]] <- .Call(class_names_, signature[[i]])
-    }
+    #}
   }
   signatures <- .Call(construct_signature_, signature)
   .Call(get_r7_method_, generic, signatures, parent.frame())
-}
-
-get_r7_method <- function(generic, class, envir) {
-  # We lookup the registered method in the S3 methods table directly, as R7 generics don't use UseMethod.
-  s3_table <- get(".__S3MethodsTable__.", envir)
-  get0(paste0(generic, ".", class), s3_table)
 }
 
 #' @rdname method
