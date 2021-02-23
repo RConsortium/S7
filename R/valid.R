f32652a44a5a96f74d2cdd2e0204766fd05ea67c
Jim Hester <james.f.hester@gmail.com>
2021-02-23 09:04:19 -0500

    Move validation functions into their own file

---
+++ R/valid.R
@@ -0,0 +1,52 @@
+#' Validation of R7 objects
+#'
+#' [validate()] calls the validation of an R7 object. This is done
+#' automatically when creating new objects (at the end of `object_new()`) and
+#' when setting any property.
+#'
+#' [valid_eventually()] disables validation of properties until the expression
+#' in `expr` is completed, then validates the object. [valid_implicitly()]
+#' does the same but does not validate the object at the end.
+#'
+#' [valid_implicitly()] should only be used rarely in performance critical code
+#' where you are certain a sequence of operations cannot produce an invalid
+#' object.
+#' @param object An R7 object
+#' @param expr An R expression
+validate <- function(object) {
+  if (!is.null(attr(object, ".should_validate"))) {
+    return(invisible(object))
+  }
+
+  obj_class <- object_class(object)
+  validator <- property(obj_class, "validator")
+
+  errors <- validator(object)
+
+  if (length(errors) > 0) {
+    msg <- sprintf("invalid '%s' object:\n%s", property(obj_class, "name"), paste0("- ", errors, collapse = "\n"))
+    stop(msg, call. = FALSE)
+  }
+
+  invisible(object)
+}
+
+#' @rdname validate
+#' @export
+valid_eventually <- function(object, fun) {
+  old <- attr(object, ".should_validate")
+  attr(object, ".should_validate") <- FALSE
+  out <- fun(object)
+  attr(out, ".should_validate") <- old
+  validate(out)
+}
+
+#' @rdname validate
+#' @export
+valid_implicitly <- function(object, fun) {
+  old <- attr(object, ".should_validate")
+  attr(object, ".should_validate") <- FALSE
+  out <- fun(object)
+  attr(out, ".should_validate") <- old
+  invisible(out)
+}
---
+++ tests/testthat/test-valid.R
@@ -0,0 +1,36 @@
+range <- class_new("range",
+  constructor = function(start, end) {
+    object_new(start = start, end = end)
+  },
+  validator = function(x) {
+    if (property(x, "end") < property(x, "start")) {
+      "`end` must be greater than or equal to `start`"
+    }
+  },
+  properties = c(start = "numeric", end = "numeric")
+)
+test_that("validate calls the validation function", {
+  obj <- range(1, 10)
+  # Use attr to set the property
+  attr(obj, "start") <- 11
+
+  expect_error(
+    validate(obj),
+    "must be greater than"
+  )
+})
+
+test_that("valid implicitly does _not_ call the validation function", {
+  obj <- range(1, 10)
+
+  obj <- valid_implicitly(
+    obj,
+    function(x) {
+      x@start <- 11
+      x
+    })
+
+  expect_error(validate(obj),
+    "must be greater than"
+  )
+})
