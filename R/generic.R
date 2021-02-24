144f5b856a7fabd17102eda743b02ffcfa3766a8
Jim Hester <james.f.hester@gmail.com>
2021-02-24 10:38:15 -0500

    Move the generic generation code to its own function
    This allows you to more easily specify your own function as the generic
    if needed

--- R/generic.R
+++ R/generic.R
@@ -1,6 +1,34 @@
 #' Define a new generic
+#'
 #' @inheritParams r7_generic
 #' @export
-generic_new <- function(name, signature) {
-  r7_generic(name = name, signature, envir = parent.frame())
+generic_new <- function(name, signature = NULL, fun = NULL) {
+  if (is.null(signature) && is.null(fun)) {
+    stop("Must call `generic_new()` with either `signature` or `fun`", call. = FALSE)
+  }
+  if (is.null(signature)) {
+    signature <- formals(fun)[1]
+  }
+  if (is.null(fun)) {
+    fun <- generic_generate(name, signature, envir = parent.frame())
+  }
+
+  r7_generic(name = name, signature, fun = fun)
+}
+
+#' Generate the body of a generic function
+#'
+#' This is used as the default to `generic_new`
+#' @param name of the generic
+#' @param signature signature of the generic
+#' @param envir environment to use as the enclosing environment of the generated generic
+#' @export
+generic_generate <- function(name, signature, envir = parent.frame()) {
+  fun <- function() NULL
+  formals(fun) <- signature
+  sig_call <- as.call(c(as.symbol("list"), lapply(names(signature), function(x) { bquote(object_class(.(arg)), list(arg = as.symbol(x)))})))
+  method_call <- as.call(c(as.call(c(as.symbol("method"), as.symbol(name), sig_call)), lapply(names(signature), as.symbol)))
+  body(fun) <- method_call
+  environment(fun) <- envir
+  fun
 }
--- R/zzz.R
+++ R/zzz.R
@@ -9,20 +9,13 @@ r7_object <- class_new(
 #' r7 generics and method objects
 #' @param name,generic The name or generic object of the generic
 #' @param signature The signature of the generic
-#' @param fun The function of the method'
-#' @param envir The environment to lookup the generic in.
+#' @param fun The function to use as the body of the generic.
 #' @export
 r7_generic <- class_new(
   name = "r7_generic",
   properties = list(name = "character", signature = "list"),
   parent = "function",
-  constructor = function(name, signature, envir = parent.frame()) {
-    fun <- function() NULL
-    formals(fun) <- signature
-    sig_call <- as.call(c(as.symbol("list"), lapply(names(signature), function(x) { bquote(object_class(.(arg)), list(arg = as.symbol(x)))})))
-    method_call <- as.call(c(as.call(c(as.symbol("method"), as.symbol(name), sig_call)), lapply(names(signature), as.symbol)))
-    body(fun) <- method_call
-    environment(fun) <- envir
+  constructor = function(name, signature, fun) {
     object_new(name = name, signature = signature, .data = fun)
   }
 )
