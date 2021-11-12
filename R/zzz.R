6558f7c7c771617ebab17c8adb76e729e4fc04b2
Hadley Wickham <h.wickham@gmail.com>
2021-11-12 17:40:09 -0600

    Put ) in correct place

--- R/zzz.R
+++ R/zzz.R
@@ -90,7 +90,7 @@ is_union <- function(x) inherits(x, "R7_union")
 
 #' @export
 print.R7_union <- function(x, ...) {
-  cat(sprintf("<R7_union>: %s", fmt_classes(class_names(x), " u "), "\n", sep = ""))
+  cat(sprintf("<R7_union>: %s", fmt_classes(class_names(x), " u ")), "\n", sep = "")
   invisible(x)
 }
 
