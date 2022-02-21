4675283b713e63aabfd7b935673d629bf40ba336
Hadley Wickham <h.wickham@gmail.com>
2022-02-21 16:57:08 -0600

    Correct order of class vector
    And re-establish setOldClass() for R7 objects. Fixes #70.

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -2,3 +2,9 @@ test_that("R7_class validates its underlying data", {
   x <- new_class("X")()
   expect_snapshot_error(R7_data(x) <- 1)
 })
+
+test_that("register S4 classes for key components", {
+  expect_s4_class(getClass("R7_object"), "classRepresentation")
+  expect_s4_class(getClass("R7_method"), "classRepresentation")
+  expect_s4_class(getClass("R7_generic"), "classRepresentation")
+})
