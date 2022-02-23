d097f6f36fbf1bb72a38247cc17fcfd508079278
Hadley Wickham <h.wickham@gmail.com>
2022-02-23 08:10:42 -0600

    Rearrange fallback tests
    Add test for self

--- tests/testthat/test-cast.R
+++ tests/testthat/test-cast.R
@@ -19,20 +19,36 @@ test_that("doesn't cast to subclass", {
   expect_error(cast(integer, casttest2), "Can't find method")
 })
 
-test_that("fallback casts work", {
-  foo1 <- new_class("foo1")
-  foo2 <- new_class("foo2", foo1)
-  obj <- cast(foo2(), foo1)
-  expect_equal(class(obj), c("foo1", "R7_object"))
-  expect_equal(object_class(obj), foo1)
+describe("fallback cast", {
+  it("can cast to own class", {
+    foo1 <- new_class("foo1")
+    foo2 <- new_class("foo2", foo1)
 
-  factor2 <- new_class("factor2", S3_factor)
-  obj <- cast(factor2(1, "x"), S3_factor)
-  expect_equal(class(obj), "factor")
-  expect_equal(object_class(factor2), NULL)
+    obj <- cast(foo2(), foo2)
+    expect_equal(class(obj), c("foo2", "foo1", "R7_object"))
+    expect_equal(object_class(obj), foo2)
+  })
 
-  character2 <- new_class("character2", "character")
-  obj <- cast(character2("x"), character)
-  expect_equal(attr(obj, "class"), NULL)
-  expect_equal(object_class(obj), NULL)
+  it("can cast to super class", {
+    foo1 <- new_class("foo1")
+    foo2 <- new_class("foo2", foo1)
+
+    obj <- cast(foo2(), foo1)
+    expect_equal(class(obj), c("foo1", "R7_object"))
+    expect_equal(object_class(obj), foo1)
+  })
+
+  it("can cast to S3 class", {
+    factor2 <- new_class("factor2", S3_factor)
+    obj <- cast(factor2(1, "x"), S3_factor)
+    expect_equal(class(obj), "factor")
+    expect_equal(object_class(factor2), NULL)
+  })
+
+  it("can cast to base type", {
+    character2 <- new_class("character2", "character")
+    obj <- cast(character2("x"), character)
+    expect_equal(attr(obj, "class"), NULL)
+    expect_equal(object_class(obj), NULL)
+  })
 })
