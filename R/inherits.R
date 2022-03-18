2c068546d96fbf1aefd473ca6f44759731d602ee
Hadley Wickham <h.wickham@gmail.com>
2022-03-18 14:01:46 -0500

    Implement R7_inherits() and check_R7_inherits()
    Fixes #193

---
+++ R/inherits.R
@@ -0,0 +1,34 @@
+#' Does this object inherit from an R7 class?
+#'
+#' * `R7_inherits()` returns `TRUE` or `FALSE`
+#' * `check_R7_inherits()` throws an error.
+#'
+#' @param x An object
+#' @param class An R7 class
+#' @param arg Argument name used in error message.
+#' @export
+#' @examples
+#' foo1 <- new_class("foo1")
+#' foo2 <- new_class("foo2")
+#'
+#' R7_inherits(foo1(), foo1)
+#' check_R7_inherits(foo1(), foo1)
+#'
+#' R7_inherits(foo1(), foo2)
+#' try(check_R7_inherits(foo1(), foo2))
+R7_inherits <- function(x, class) {
+  if (!inherits(class, "R7_class")) {
+    stop("`class` is not an <R7_class>")
+  }
+
+  inherits(x, "R7_object") && inherits(x, R7_class_name(class))
+}
+
+#' @export
+#' @rdname R7_inherits
+check_R7_inherits <- function(x, class, arg = deparse(substitute(x))) {
+  if (!R7_inherits(x, class)) {
+    stop(sprintf("`%s` is not an %s", arg, class_desc(class)), call. = FALSE)
+  }
+  invisible()
+}
---
+++ tests/testthat/_snaps/inherits.md
@@ -0,0 +1,16 @@
+# checks that input is a class
+
+    Code
+      R7_inherits(1:10, "x")
+    Error <simpleError>
+      `class` is not an <R7_class>
+
+# throws informative error
+
+    Code
+      foo1 <- new_class("foo1")
+      foo2 <- new_class("foo2")
+      check_R7_inherits(foo1(), foo2)
+    Error <simpleError>
+      `foo1()` is not an <foo2>
+
