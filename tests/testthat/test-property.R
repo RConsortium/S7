3325169c1351ab468c6f775f71c819e5c6752f93
Tomasz Kalinowski <tomasz@posit.co>
2023-02-14 23:11:05 -0500

    update snapshot tests for new (old) `@` error

--- tests/testthat/test-property.R
+++ tests/testthat/test-property.R
@@ -17,14 +17,12 @@ describe("property retrieval", {
     expect_equal(obj@x, 1)
   })
 
-  if (getRversion() < "4.3.0") {
-    it("falls back to `base::@` for non-S7 objects", {
-      expect_snapshot(error = TRUE, {
-        "foo"@blah
-        NULL@blah
-      })
+  it("falls back to `base::@` for non-S7 objects", {
+    expect_snapshot(error = TRUE, {
+      "foo"@blah
+      NULL@blah
     })
-  }
+  })
 })
 
 describe("prop setting", {
@@ -88,12 +86,7 @@ describe("prop setting", {
 
   it("falls back to `base::@` for non-S7 objects", {
     x <- "foo"
-    if (getRversion() < "4.3.0") {
-      expect_error(x@blah <- "bar", "is not a slot in class")
-    } else {
-      expect_no_error(x@blah <- "bar")
-      expect_identical(x@blah, "bar")
-    }
+    expect_error(x@blah <- "bar", "is not a slot in class")
   })
 })
 
