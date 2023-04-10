be0e1ebe80c3ce8926556cf853557cb23589ff43
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-04-10 08:52:44 -0400

    workaround for `.AtNames()` not on 4.3 branch yet

--- R/property.R
+++ R/property.R
@@ -274,9 +274,11 @@ prop_names <- function(object) {
   }
 }
 
-#' @rawNamespace if (getRversion() >= "4.3.0") S3method(.AtNames,S7_object)
+# .AtNames not exported on r-devel yet, causes installation failure
+#' @rawNamespace if (getRversion() >= "4.3.0" && !is.null(asNamespace("utils")$.AtNames)) S3method(.AtNames,S7_object)
 .AtNames.S7_object <- function(x, pattern = "") {
-  utils::findMatches(pattern, prop_names(x))
+  # utils::findMatches gives `R CMD check` warning on current r-devel
+  asNamespace("utils")$findMatches(pattern, prop_names(x))
 }
 
 #' @rdname prop_names
