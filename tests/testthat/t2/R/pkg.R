67d2177c55ed0bdf8db9ed56ffe86e7ee2733c6a
Hadley Wickham <h.wickham@gmail.com>
2023-07-18 09:01:26 -0500

    Test method registration for external S7 generic
    And use correct environment for generic

--- tests/testthat/t2/R/pkg.R
+++ tests/testthat/t2/R/pkg.R
@@ -1,7 +1,6 @@
 foo <- S7::new_external_generic("t1", "foo", "x")
 S7::method(foo, S7::class_character) <- function(x) "foo"
 
-bar <- S7::new_external_generic("t0", "bar", "x")
 S7::method(bar, S7::class_character) <- function(x) "bar"
 
 .onLoad <- function(libname, pkgname) {
