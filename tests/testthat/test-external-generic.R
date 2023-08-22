b5057db811ce182f5b2055803add379dfcc1ca6e
Hadley Wickham <h.wickham@gmail.com>
2023-08-22 14:50:56 -0500

    Use string for version comparison

--- tests/testthat/test-external-generic.R
+++ tests/testthat/test-external-generic.R
@@ -30,7 +30,7 @@ test_that("displays nicely", {
 test_that("new_method works with both hard and soft dependencies", {
   # NB: Relies on installed S7
 
-  skip_if(getRversion() < 4.1 && Sys.info()[["sysname"]] == "Windows")
+  skip_if(getRversion() < "4.1" && Sys.info()[["sysname"]] == "Windows")
   skip_if(quick_test())
 
   tmp_lib <- tempfile()
