4c81d2b9d35d2df1bd48f72c1fe8799acf1c2014
Jermiah Joseph <jermiahjoseph98@gmail.com>
2026-04-11 11:40:23 -0400

    Remove redundant "error" from S7_error_method_not_found class vector
    errorCondition() already appends "error" and "condition", so passing
    "error" explicitly in the class argument results in a duplicate.

--- R/method-dispatch.R
+++ R/method-dispatch.R
@@ -2,7 +2,7 @@
 method_lookup_error <- function(name, args) {
   types <- vcapply(args, obj_desc)
   msg <- method_lookup_error_message(name, types)
-  cnd <- errorCondition(msg, class = c("S7_error_method_not_found", "error"))
+  cnd <- errorCondition(msg, class = "S7_error_method_not_found")
   stop(cnd)
 }
 
