26b7251d77d95a527c1fba28507d12df8335ae26
Michael Lawrence <lawremi@gmail.com>
2026-06-02 05:31:53 -0700

    tweaks to validObject hooks to support additional inheritance patterns

--- R/S4.R
+++ R/S4.R
@@ -195,7 +195,7 @@ S4_register_subclass <- function(class, env) {
       S4Class = S4_register_prototype_class(class, where),
       where = where
     )
-    methods::setValidity(subclasses[1L], S4_validate, where = where)
+    methods::setValidity(subclasses[1L], S4_validate_old_class, where = where)
     methods::setMethod(
       "initialize",
       subclasses[1L],
@@ -249,6 +249,7 @@ S4_register_with_props <- function(class, env) {
     prototype = methods::prototype(S7_class = class),
     where = where
   )
+  methods::setValidity(class_name, S4_validate_shim, where = where)
 
   class_name
 }
@@ -285,7 +286,21 @@ S4_subclasses <- function(class) {
   character()
 }
 
-S4_validate <- function(object) {
+S4_validate_old_class <- function(object) {
+  if (isS4(object)) {
+    # covered by S4_validate_shim()
+    return(TRUE)
+  }
+
+  S4_validate_from(object)
+}
+
+S4_validate_shim <- function(object) {
+  parent <- S4_ancestor(S7_class(object)) %||% S7_object
+  S4_validate_from(object, parent = parent)
+}
+
+S4_validate_from <- function(object, parent = NULL) {
   if (!S7_inherits(object)) {
     return(sprintf(
       "object with S4 class %s is not an S7 object",
@@ -295,7 +310,7 @@ S4_validate <- function(object) {
 
   tryCatch(
     {
-      validate(object)
+      validate_from(object, parent = parent)
       TRUE
     },
     error = function(cnd) conditionMessage(cnd)
