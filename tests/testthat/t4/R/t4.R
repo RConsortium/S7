84451cf747613e68caf798e8a1d3aab75de3cdfb
Hadley Wickham <h.wickham@gmail.com>
2026-06-03 11:30:02 -0400

    No longer need to register onLoad

--- tests/testthat/t4/R/t4.R
+++ tests/testthat/t4/R/t4.R
@@ -1,5 +1,6 @@
 #' @export
 t4_class <- S7::new_class("t4_class", package = "t4")
+S7::S4_register(t4_class)
 
 # Register an S7 method for an S3 generic defined in another package (t3).
 S7::method(t3_s3, t4_class) <- function(x) "s3-dispatch"
@@ -9,7 +10,6 @@ S7::S4_register(t4_class)
 S7::method(t3_s4, t4_class) <- function(x) "s4-dispatch"
 
 .onLoad <- function(libname, pkgname) {
-  S7::S4_register(t4_class)
   S7::methods_register()
 }
 
